739712a5456c6c92897170324cf27aa543685559
Thomas W <tomwim@users.noreply.github.com>
2021-03-16 15:31:35 +0100

    feat: course controller is initiated with default on scene setup

--- Editor/CourseController/CourseControllerSceneSetup.cs
+++ Editor/CourseController/CourseControllerSceneSetup.cs
@@ -1,4 +1,8 @@
-﻿namespace Innoactive.CreatorEditor.UX
+﻿using Innoactive.Creator.Unity;
+using Innoactive.Creator.UX;
+using UnityEngine;
+
+namespace Innoactive.CreatorEditor.UX
 {
     /// <summary>
     /// Will be called on OnSceneSetup to add the course controller menu.
@@ -14,7 +18,10 @@
         /// <inheritdoc />
         public override void Setup()
         {
-            SetupPrefab("[COURSE_CONTROLLER]");
+            GameObject courseController = FindPrefab("[COURSE_CONTROLLER]");
+            courseController.name = courseController.name.Replace("(Clone)", string.Empty);
+            courseController.GetOrAddComponent<CourseControllerSetup>().ResetToDefault();
+            Object.Instantiate(courseController);
         }
     }
 }
